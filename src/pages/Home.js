edae405ebbd00e6e3c5c3af4769921e11788b21b
Chris <chrisg1821@gmail.com>
2022-01-14 00:02:45 -0800

    I think I have worked out how many different states I will need, and how to make sure my data flows to the cards. Tomorrow going to figure out how to use 2 different api keys in one .env

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { Container, Col, Form, Button, Row, Card, CardColumns } from 'react-bootstrap';
-import { searchCity, searchUv } from '../utils/API';
+import { Container, Col, Form, Button, Row, Card } from 'react-bootstrap';
+import { searchCity, getSevenDayWeatherInfo, getBackgroundImage } from '../utils/API';
 
 const HomePage = () => {
     const [searchInput, setSearchInput] = useState('');
+    const [searchedCity, setSearchedCity] = useState([]);
+    const [forecastData, setForecastData] = useState([]);
+    const [backgroundImage, setBackgroundImage] = useState([])
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
@@ -20,29 +23,57 @@ const HomePage = () => {
             }
 
             const searchData = await searchResponse.json();
-            const cityData = {
+            const cityData = ({
                 cityId: searchData.id,
                 cityName: searchData.name,
                 lat: searchData.coord.lat,
                 lon: searchData.coord.lon,
-                
-            };
+            });
+            const todayForecast = ([{
+                day: new Date(searchData.dt * 1000).toLocaleDateString("en-US", {weekday: 'long'}),
+                date: new Date(searchData.dt * 1000).toLocaleDateString("en-US"),
+                temp: searchData.main.temp,
+                humidity: searchData.main.humidity,
+                weatherDescription: searchData.weather[0].description,
+                weatherIcon: searchData.weather[0].icon,
+            }])
+            setSearchedCity(cityData);
+            setSearchInput('')
 
-            const uvResponse = await searchUv(cityData.lat, cityData.lon);
-            const uvData = await uvResponse.json()
-            cityData.uvi = uvData.current.uvi
+            getSevenDayWeatherInfo(cityData.lat, cityData.lon)
+                .then(response => response.json())
+                .then(data => {
+                    const { daily } = data
+                    const sevenDayForecast = daily.map((day) => ({
+                        day: new Date(day.dt * 1000).toLocaleDateString("en-US", {weekday: 'long'}),
+                        date: new Date(day.dt *1000).toLocaleDateString("en-US"),
+                        tempMin: day.temp.min,
+                        tempMax: day.temp.max,
+                        humidity: day.humidity,
+                        weatherDescription: day.weather[0].description,
+                        weatherIcon: day.weather[0].icon
+                    }));
+                    const forecast = todayForecast.concat(sevenDayForecast)
+                    setForecastData(forecast);
+                });
 
-            console.log(cityData)
-            console.log(uvData)
+            getBackgroundImage(cityData.cityName)
+                .then(response => response.json())
+                .then(data => {
+                    const cityImage = data
+                    console.log(cityImage)
+                });
         } catch (err) {
             console.error(err);
         }
     }
+    console.log(forecastData)
+
     return (
         <Container className='py-4'>
             <div className='p-5 mb-4 bg-light rounded-3'>
                 <Container fluid={true} className='py-5'>
-                        <h1 className='display-5 fw-bold'> Search for a city: </h1>
+                    <h1 className='display-5 fw-bold'> Search for a city: </h1>
                         <Form onSubmit={handleFormSubmit}>
                             <Row>
                                 <Col xs={12} md={8}>
@@ -65,6 +96,24 @@ const HomePage = () => {
                         </Form>
                 </Container>
             </div>
+            <Container className='py-5'>
+                <h2 className='display-5 fw-bold'> {searchedCity.cityName} </h2>
+                <h3 className='display-5 fw-bold'> {searchedCity.currentTemp} </h3>
+                    <Row>
+                    {forecastData.slice(0,5).map((day) => {
+                        return (
+                        <Col key={day.date}>
+                            <Card>
+                                <Card.Title>
+                                {day.date === new Date().toLocaleDateString("en-US") ? 'Today' : day.date} <br></br> {day.day}
+                                </Card.Title>
+                                
+                            </Card>
+                        </Col>
+                        )
+                    })}
+                    </Row>
+            </Container>
         </Container>
     )
 };
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,16 @@
-const { REACT_APP_APIKEY } = process.env;
+const { REACT_APP_APIKEY, REACT_APP_APIKEY2 } = process.env;
 
 export const searchCity = (city) => {
-    var endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${REACT_APP_APIKEY}`
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${REACT_APP_APIKEY}`
     return fetch(endpoint);
 };
 
-export const searchUv = (lat, lon) => {
-    var getUV = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${REACT_APP_APIKEY}`
-    return fetch(getUV);
+export const getSevenDayWeatherInfo = (lat, lon) => {
+    const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${REACT_APP_APIKEY}`
+    return fetch(endpoint);
+};
+
+export const getBackgroundImage = (city) => {
+    const endpoint = `https://api.unsplash.com/photos?query=${city}&client_id=${REACT_APP_APIKEY2}`
+    return fetch(endpoint);
 }
